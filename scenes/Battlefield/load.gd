cb0de5b3d12f2d5ea4ab28a57d955bdb7aba3a47
Ryan Masters <alldevallday@gmail.com>
2021-04-19 09:21:19 -0400

    Update load.gd
    - beginning to make walls and corners work correctly

--- scenes/Battlefield/load.gd
+++ scenes/Battlefield/load.gd
@@ -4,58 +4,76 @@ extends Node
 var map = {
 	0: {
 		'label': 'StandIn_BasicGroundTile',
-		'top': false
+		'wall': false
 	},
 	1: {
 		'label': 'StandIn_BarrelObstacle',
-		'top': true
+		'wall': false
 	},
 	2: {
 		'label': 'StandIn_GridGroundTile',
-		'top': false
+		'wall': false
 	},
 	3: {
 		'label': 'StandIn_SpikeGroundTile',
-		'top': false
+		'wall': false
 	},
 	4: {
 		'label': 'StandIn_WindowlessCornerTile',
-		'top': false
+		'wall': true
 	},
 	5: {
 		'label': 'StandIn_WindowlessWallTile',
-		'top': false
+		'wall': true
 	},
 }
 
 # tiles of the battlefield by row
 var level = [
 	[
-		0,0,2,0,0
+		4,5,5,5,4
 	],
 	[
-		0,3,0,3,0
+		5,3,0,3,5
 	],
 	[
-		2,0,1,0,2
+		5,0,1,0,5
 	],
 	[
-		0,3,0,3,0
+		5,3,0,3,5
 	],
 	[
-		0,0,2,0,0
+		4,5,5,5,4
 	]
 ]
 
 # adds a tile to the scene
-func add_tile(tileIndex, rowIndex, mapped):
+func add_tile(tileIndex, rowIndex, mapped, position):
+	# duplicate a hidden asset node
 	var node = self.get_node("WorldEnvironment/" + mapped.label).duplicate()
+	
+	# position the new node
 	var x = Vector3(1, 0, 0)
 	var y = Vector3(0, 1, 0)
 	var z = Vector3(0, 0, 1)
 	var origin = Vector3(tileIndex, 0, rowIndex)
 	node.transform = Transform(x, y, z, origin)
 	node.visible = true
+	
+	# if this is a wall, we need to rotate it some
+	if mapped.wall:		
+		# figure out how to rotate it
+		var rotateMap = {
+			'top' : PI/2,
+			'bottom' : PI/2,
+			'left' : PI/2,
+			'right' : PI/2
+		}
+		
+		var axis = Vector3(0, 1, 0)
+		node.rotate(axis, rotateMap[position])
+		
+	# add the node
 	self.add_child(node)
 
 # Called when the node enters the scene tree for the first time.
@@ -65,12 +83,10 @@ func _ready():
 			# figure out which tile to use
 			var mapped = map[level[rowIndex][tileIndex]]
 			
-			# if this is a top tile, add a basic tile under it
-			if mapped.top:
-				add_tile(tileIndex, rowIndex, map[0])
+			var position = 'top'
 			
 			# add the tile
-			add_tile(tileIndex, rowIndex, mapped)
+			add_tile(tileIndex, rowIndex, mapped, position)
 			
 
 
