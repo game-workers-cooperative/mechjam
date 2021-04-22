36c9668e28b9441f675ab220b32a3ecbbac4d57c
Akshat Shukla <shuklaakshatascom@gmail.com>
2021-04-22 11:47:37 +0530

    Added HUD and tweening

--- scenes/Battlefield/load.gd
+++ scenes/Battlefield/load.gd
@@ -1,8 +1,15 @@
 extends Node
 
+const CommandBlock = preload("res://objects/ui/CommandBlock.tscn")
+
 var map
 var player
 var enemy
+
+var commands = []
+
+onready var command_editor = $HUD/CommandEditor
+onready var command_palette = $HUD/CommandPalette
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,7 +31,54 @@ func _ready():
 	
 	# build the map
 	map.build_map(self)
+	
+	# Connect each button's pressed signal to this node
+	for button in command_palette.get_children():
+		button.connect("command_selected", self, "_on_command_selected")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func execute_commands():
+	for command in commands:
+		print(command)
+		player.call(command.method, command.parameters)
+		yield(player, "move_finished")
+	
+	clear_children(command_editor)
+	commands = []
+
+# Utils
+
+# Changes the index property of the blocks to be correct if any block is removed to avoid discontinued indexes
+func check_block_index():
+	var i = 0
+	for block in command_editor.get_children():
+		block.index = i
+		i += 1
+
+# Remove all the children from the parent node
+func clear_children(parent):
+	for child in parent.get_children():
+		parent.remove_child(child)
+
+# Signal Connections
+
+# Adds the Command to the command editor and command list
+func _on_command_selected(button):
+	var block = CommandBlock.instance()
+	block.connect("block_selected", self, "_on_block_selected")
+	block.set_text(button.get_text())
+	block.index = commands.size()
+	commands.append(button.command)
+	command_editor.add_child(block)
+
+
+# Removes the command from the editor and list
+func _on_block_selected(block):
+	commands.remove(block.index)
+	block.queue_free()
+	check_block_index()
+	
+#	print(commands)
+
+# Executes the commands(Starts Battle Phase)
+func _on_StartBtn_pressed() -> void:
+	execute_commands()
